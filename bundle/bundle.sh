4ec15f9dc5573426cccd56fe882532740247dd00
obr-suporte <obr-suporte@ceiasp101.crocodile-kitefin.ts.net>
2025-03-17 13:53:10 -0300

    baiano

--- bundle/bundle.sh
+++ bundle/bundle.sh
@@ -11,10 +11,10 @@ rm -rf ./bundle/dist
 
 onefile="--onefile"
 
-# bundle app, dependencies and data files into single executable
-
+# bundle app, dependencies, and data files into a single executable
 pyinstaller \
---add-data './world/commons/robots:world/commons/robots' \
+--onefile \
+--add-binary '/usr/lib/x86_64-linux-gnu/libm.so.6:lib/' \
 --add-data './behaviors/slot/common:behaviors/slot/common' \
 --add-data './behaviors/slot/r0:behaviors/slot/r0' \
 --add-data './behaviors/slot/r1:behaviors/slot/r1' \
@@ -27,7 +27,6 @@ pyinstaller \
 ${onefile} --distpath ./bundle/dist/ --workpath ./bundle/build/ --noconfirm --name peq Run_teamCEIA.py
 
 # start.sh
-
 cat > ./bundle/dist/start.sh << EOF
 #!/bin/bash
 export OMP_NUM_THREADS=1
@@ -41,7 +40,6 @@ done
 EOF
 
 # start_penalty.sh
-
 cat > ./bundle/dist/start_penalty.sh << EOF
 #!/bin/bash
 export OMP_NUM_THREADS=1
@@ -54,7 +52,6 @@ port=\${2:-3100}
 EOF
 
 # start_fat_proxy.sh
-
 cat > ./bundle/dist/start_fat_proxy.sh << EOF
 #!/bin/bash
 export OMP_NUM_THREADS=1
@@ -68,16 +65,13 @@ done
 EOF
 
 # kill.sh
-
 cat > ./bundle/dist/kill.sh << EOF
 #!/bin/bash
-pkill -9 -e fcp
+pkill -9 -e peq
 EOF
 
 # execution permission
-
 chmod a+x ./bundle/dist/start.sh
 chmod a+x ./bundle/dist/start_penalty.sh
 chmod a+x ./bundle/dist/start_fat_proxy.sh
 chmod a+x ./bundle/dist/kill.sh
-
